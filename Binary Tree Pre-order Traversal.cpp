fe15c924f555060e2c261f8c57582f1146e39d42
Suleman-Arshad <sulemanarshad0682@gmail.com>
2025-07-15 15:00:13 +0500

    Add files via upload

---
+++ Binary Tree Pre-order Traversal.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+node *BuildTree(node *root)
+{
+    int d;
+    cout << "Enter data : ";
+    cin >> d;
+    root = new node(d);
+    if (d == -1)
+    {
+        return NULL;
+        /* code */
+    }
+    cout << "Enter left node of " << d << endl;
+    root->left = BuildTree(root->left);
+    cout << "Enter right node of " << d << endl;
+    root->right = BuildTree(root->right);
+    return root;
+}
+vector<int> arr;
+vector<int> Preorder_traversal(node *root)
+{
+
+    if (root == NULL)
+    {
+        return arr;
+    }
+    arr.push_back(root->data);
+    Preorder_traversal(root->left);
+    Preorder_traversal(root->right);
+    return arr;
+}
+int main()
+{
+    node *root = NULL;
+    vector<int> arr;
+    root = BuildTree(root);
+    cout << "Inorder Traversal of Binary Tree\n";
+    arr = Preorder_traversal(root);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << "  ";
+        /* code */
+    }
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
